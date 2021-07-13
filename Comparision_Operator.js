2c61dc8cb396ccb6bd0d848492b485e5bfa02b68
Shubham Rawat <46570973+shubhamrawat140798@users.noreply.github.com>
2021-07-13 15:01:17 +0530

    Create Comparision_Operator.js

---
+++ Comparision_Operator.js
@@ -0,0 +1,9 @@
+console.log("0==\"0\"",0=="0");
+console.log("\"\"==\"0\"",""=="0");
+console.log("false==0",false==0);
+console.log("false==\"0\"",false=="0");
+console.log("\"\"!=0",""!=0);
+console.log("0!=\"0\"",0!="0");
+console.log("\"\"!=0",""!=0);
+console.log("false !=0",false!=0);
+console.log("false!=\"\"",false!="");
